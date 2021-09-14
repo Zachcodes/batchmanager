97afbbfde95466ca7b1d79fb636154e626bc8e89
Zach Springer <zaspringer@gmail.com>
2021-09-14 15:42:09 -0600

    added some more testing

--- batchManager.test.js
+++ batchManager.test.js
@@ -1,5 +1,11 @@
 const batchManager = require('./lib/BatchManager');
 
+const createQueueItem = resolveMessage => {
+    return async () => {
+        console.log(resolveMessage)
+    }
+}
+
 describe(('BatchManager Base Class Functionality'), () => {
     let manager;
     afterEach(() => {
@@ -29,5 +35,47 @@ describe(('BatchManager Base Class Functionality'), () => {
         expect(manager.processQueue.length).toBe(0);
         expect(manager.errorQueue.length).toBe(0);
         expect(manager.meta.queueIntervalId).toBeNull();
+    });
+
+    test('sleep should resolve after the specified amount of ms', () => {
+        manager = batchManager();
+        const startTime = new Date().getTime();
+        const ms = 1000;
+        return manager.sleep(ms).then( _ => {
+            const endTime = new Date().getTime();
+            expect(endTime - startTime).toBeGreaterThanOrEqual(ms);
+        });
+    });
+
+    test('sleep should stagger with random time interval', () => {
+        manager = batchManager();
+        const startTime = new Date().getTime();
+        const ms = 1000;
+        return manager.sleep(ms, true).then( _ => {
+            const endTime = new Date().getTime();
+            expect(endTime - startTime).toBeLessThanOrEqual(ms);
+        });
+    });
+});
+
+describe.only(('SequentialManager Functionality'), () => {
+    let manager;
+    afterEach(() => {
+        manager.retire();
+    });
+
+    test('addToQueue should add an item to the process queue', () => {
+        manager = batchManager();
+        manager.addToQueue([createQueueItem('ran the callback')]);
+        expect(manager.promiseQueue.length).toBe(1);
+    });
+
+    test('queue items are processed sequentially', () => {
+        manager = batchManager();
+        manager.addToQueue([createQueueItem('running callback 1')]);
+        manager.addToQueue([createQueueItem('running callback 2')]);
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(), 3000)
+        })
     })
 });
